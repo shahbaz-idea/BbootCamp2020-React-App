837a19f5b892b257010824438885e8ce6ee2b404
Shahbaz Akbar <shahbaz.idea@outlook.com>
2021-04-24 14:55:03 +0500

    family

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Family from './Family';
 
 function App() {
   return (
     <div className="App">
       <div className="App-header">
       <h1>I Love my Family!</h1>
+      <Family/>
     </div></div>
   );
 }
---
+++ src/Family.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Family(){
+    return(
+        <div>
+            <h1>Beautiful Wife</h1>
+            <h2>Zain ul Abideen</h2>
+            <h2>Abdullah</h2>
+            <h2>Muaaz</h2>
+        </div>
+    )
+}
+export default Family;
