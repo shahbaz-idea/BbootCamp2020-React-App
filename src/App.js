22a36da982d9422870651247e032e97e9c8e86c9
Shahbaz Akbar <shahbaz.idea@outlook.com>
2021-04-24 15:57:23 +0500

    Update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import './App.css';
-import Family from './Family';
+import Dinner from './Dinner';
+// import Family from './Family';
 
 function App() {
   return (
     <div className="App">
       <div className="App-header">
-      <h1>I Love my Family!</h1>
-      <Family/>
+      {/* <h1>I Love my Family!</h1> 
+      <Family/> */}
+      <h1>I Love Food!</h1>
+      <hr/>
+      <Dinner dishName="Biryani" sweetDish="Kheer" />
+      <hr/>
+      <Dinner dishName="Nehari" sweetDish="Gajrela" />
+      <hr/>
+      <Dinner dishName="Karahi" sweetDish="Russ Malai" />
     </div></div>
   );
 }
