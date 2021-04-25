4b4778430a4f00f83a25110ebde3d04639bdeb8d
Shahbaz Akbar <shahbaz.idea@outlook.com>
2021-04-25 22:23:04 +0500

    Counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import Dinner from './Dinner';
-// import Family from './Family';
+//import Dinner from './Dinner';
+//import Family from './Family';
+import Message from './Mesage';
 
-function App() {
+export default function App() {
+  let [count, setCount] = useState(1); 
   return (
     <div className="App">
       <div className="App-header">
       {/* <h1>I Love my Family!</h1> 
       <Family/> */}
-      <h1>I Love Food!</h1>
+      {/* <h1>I Love Food!</h1>
       <hr/>
       <Dinner dishName="Biryani" sweetDish="Kheer" />
       <hr/>
       <Dinner dishName="Nehari" sweetDish="Gajrela" />
       <hr/>
-      <Dinner dishName="Karahi" sweetDish="Russ Malai" />
+      <Dinner dishName="Karahi" sweetDish="Russ Malai" /> */}
+      {/* <h1>Value of counter variable is: {count}</h1> */}
+      <Message counter={count} />
+      <br/>
+      <button onClick={ () => setCount( ++count
+        ) } >Update Counter</button>
     </div></div>
   );
 }
 
-export default App;
+//export default App;
---
+++ src/Mesage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Message(props){
+    return(
+        <h3>Value of counter variable is: {props.counter}</h3>
+    )
+}
